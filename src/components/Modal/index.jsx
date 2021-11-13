1897b801f86d9f4050aa34ddb43f92fc0f849712
GL-19 <loiolagl19@gmail.com>
2021-11-12 22:00:24 -0300

    Passagem dos styles, services, routes e App para ts

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,7 +1,7 @@
-import { Component } from 'react';
-import ReactModal from 'react-modal';
+import { useEffect, useState } from "react";
+import ReactModal from "react-modal";
 
-class Modal extends Component {
+/* class Modal extends Component {
   constructor(props) {
     super(props);
 
@@ -53,6 +53,44 @@ class Modal extends Component {
       </ReactModal>
     );
   }
-};
+}; */
+
+function Modal({ children, setIsOpen, isOpen }) {
+	const [modalStatus, setModalStatus] = useState(isOpen);
+
+	useEffect(() => {
+		console.log(isOpen);
+		setModalStatus(isOpen);
+	}, [isOpen]);
+
+	return (
+		<ReactModal
+			shouldCloseOnOverlayClick={!false}
+			onRequestClose={setIsOpen}
+			isOpen={modalStatus}
+			ariaHideApp={false}
+			style={{
+				content: {
+					top: "50%",
+					left: "50%",
+					right: "auto",
+					bottom: "auto",
+					marginRight: "-50%",
+					transform: "translate(-50%, -50%)",
+					background: "#F0F0F5",
+					color: "#000000",
+					borderRadius: "8px",
+					width: "736px",
+					border: "none",
+				},
+				overlay: {
+					backgroundColor: "#121214e6",
+				},
+			}}
+		>
+			{children}
+		</ReactModal>
+	);
+}
 
 export default Modal;
